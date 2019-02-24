812ba7becd7dc70e99896b431c6ced0b8fdb201d
Hamza <hamzayaghi5@gmail.com>
2019-02-24 09:17:48 +0200

    add slider and website

---
+++ app/Http/Controllers/Slider/SliderController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Slider;
+use Illuminate\Http\Request;
+
+class SliderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $sliders=Slider::get_all();
+        return view('admin.slider.index',compact('sliders'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.slider.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+      {   
+             $data=$request->all();
+        $main_title=$data['main_title'];
+        $sub_title=$data['sub_title'];
+ 
+      if($request->file('img_url')!= null){
+
+            $path;
+            if(request()->file('img_url')->isValid()){
+                $path = $request->file('img_url')->storeAs('public', time().'.jpg');
+                $img_url=str_replace('public/', '', $path);
+                if(empty($path)){
+                    return response()->json([],400);
+                }
+
+            }
+        Slider::slider_insert($category_id,$title,$description,$img_url);
+         return redirect('/admin/products/index');
+    }
+    return response()->json([],400);
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Slider  $slider
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Slider $slider)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Slider  $slider
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Slider $slider)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Slider  $slider
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Slider $slider)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Slider  $slider
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Slider $slider)
+    {
+        //
+    }
+}
